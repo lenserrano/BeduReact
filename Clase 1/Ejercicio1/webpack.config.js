2a2faa89ab36bfce4925c04bae9b480629043170
Len Serrano <len.serrano.g@gmail.com>
2019-11-23 13:10:50 -0600

    añadí segunda sesión

---
+++ Clase 1/Ejercicio1/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+    entry : './src/index.js',
+    output:{
+        path : path.resolve(__dirname, 'dist'),
+        filename : 'bundle.js'
+    },
+    module : {
+        rules : [
+            {
+                test : /\.s[ac]ss$/i,
+                use : [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    plugins : [new HtmlWebpackPlugin()]
+}
